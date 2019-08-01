80318d076cbedda2ab5a80b624f1a146a5623c70
Christian Orellana <christianorellana097@gmail.com>
2019-08-01 16:44:15 -0400

    Revert "init"
    This reverts commit 5d5f71743d180d59766447e67104a58005e2aeb7.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,14 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private Transform playertf; //player's transform
+    private int score;
     private float halfHeight; //half character's height
     private bool canJump;
     public float speed;
     public float jumpForce;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
     public float stopwatch;
     public Animator animator;
 
@@ -20,17 +24,19 @@ public class PlayerController : MonoBehaviour
     {
         playertf = GetComponent<Transform>();
         halfHeight = 0.5f;
-        rb2d = GetComponent<Rigidbody2D>();  
+        rb2d = GetComponent<Rigidbody2D>();
+        score = 0;
+        scoreText.text = "Coins: 0 :(";
+        winText.text = "";
+        livesText.text = "Lives: 3";    
         canJump = true;
 
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("escape"))
-        {
-            Application.Quit();
-        }
+        animator.SetFloat("Speed", rb2d.velocity.x);
     }
 
     void FixedUpdate()
@@ -45,19 +51,39 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.GetComponent<Collider2D>().tag == "PickUp"){
+            other.gameObject.SetActive(false);
+            UpdateScore();
+        }
+    }
+
     void OnCollisionStay2D(Collision2D other){
+        Debug.DrawLine(playertf.position, other.GetContact(0).point, Color.green, 1.0f,false);
 
         if(rb2d.velocity.y == 0)
         {
             canJump = true;
+            animator.SetBool("IsJumping", false);
+
         }
         if(Input.GetKey("space"))
         {
             if(canJump == true)
             {
                 canJump = false;
+                animator.SetBool("IsJumping", true);
                 Jump();
             }
+        }
+    }
+
+    void UpdateScore()
+    {
+        score++;
+        scoreText.text = "Coins: " + score.ToString();
+        if(score >= 5){
+            winText.text = "You got some coins... you win, I guess...";
         }
     }
 
